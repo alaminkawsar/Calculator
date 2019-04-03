8b62ae6cd9809b96a870291dee780fe560c1bd13
Al Amin Kawsar <alaminkawsar8643@gmail.com>
2019-04-03 10:09:27 +0600

    Main method For Calculator

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator{
+	public static void main(String arg[]){
+		double firstvalue=Double.parseDouble(args[0]);
+		char Operator = args[1];
+		double secondValue=Double.parseDouble(args[2]);
+	}
+}
