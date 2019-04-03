b112ff9809f87b0efea2a1b2183d84c64cfc68c2
Ibrahim <mmibrahim.cseru@gmail.com>
2019-04-03 10:36:23 +0600

    additionMethod declared with three parameter.First and third parameter is double type which will take number.Second parameter is character type which will take operator sign.

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,11 @@
 public class Calculator{
 	public static void main(String arg[]){
-		double firstvalue=Double.parseDouble(args[0]);
+		double firsthttps://github.com/alaminkawsar/Calculator.gitalue=Double.parseDouble(args[0]);
 		char Operator = args[1];
 		double secondValue=Double.parseDouble(args[2]);
+	}
+
+	public int additionMethod(int firstvalue,char Operator,int secondValue){
+
 	}
 }
